0d6f0467c29a1e6b19ea716dd4ab73120515d25f
starkend <danstarken@gmail.com>
2019-12-22 13:05:24 -0600

    Added Kafka Configuration class

--- src/main/java/com/starkend/kafkatest/KafkatestApplication.java
+++ src/main/java/com/starkend/kafkatest/KafkatestApplication.java
@@ -18,22 +18,4 @@ public class KafkatestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkatestApplication.class, args);
 	}
-
-	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate() {
-		ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(senderProps());
-		return new KafkaTemplate<>(pf);
-	}
-
-	private Map<String, Object> senderProps() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return props;
-	}
 }
