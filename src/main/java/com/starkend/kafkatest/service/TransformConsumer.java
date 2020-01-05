4c32428f85679fe1fd5045bdd6548d01c93834e8
starkend <danstarken@gmail.com>
2020-01-05 10:52:06 -0600

    Added Message Consumer to transform a message payload

---
+++ src/main/java/com/starkend/kafkatest/service/TransformConsumer.java
@@ -0,0 +1,22 @@
+package com.starkend.kafkatest.service;
+
+import com.starkend.kafkatest.topic.KafkaTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransformConsumer {
+
+    private final Logger logger = LoggerFactory.getLogger(TransformConsumer.class);
+
+    @KafkaListener(topics = "test", groupId = "group_id")
+    public void consume(String message) {
+        logger.info(String.format("$$ -> TransformConsumer Original Message -> %s from Topic --> %s", message, KafkaTopic.TEST.getName()));
+
+        String transformMessage = message.toUpperCase();
+
+        logger.info(String.format("$$ -> TransformConsumer Transformed Message -> %s from Topic --> %s", transformMessage, KafkaTopic.TEST.getName()));
+    }
+}
