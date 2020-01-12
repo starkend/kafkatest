0a446d436dbfdcd04555d50ca2decca29219f694
starkend <danstarken@gmail.com>
2020-01-12 14:43:36 -0600

    Removed explicit config definition to test config set in properties file.

--- src/main/java/com/starkend/kafkatest/KafkaConfig.java
+++ src/main/java/com/starkend/kafkatest/KafkaConfig.java
@@ -21,29 +21,29 @@ import java.util.stream.Stream;
 @SpringBootConfiguration
 public class KafkaConfig {
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(senderProps());
-        AdminClient adminClient = KafkaAdminClient.create(senderProps());
-
-        CreateTopicsResult res = adminClient.createTopics(
-                Stream.of(KafkaTopic.values()).map(
-                        name -> new NewTopic(name.getName(), 1, (short) 1)
-                ).collect(Collectors.toList())
-        );
-
-        return new KafkaTemplate<>(pf);
-    }
-
-    private Map<String, Object> senderProps() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return props;
-    }
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate() {
+//        ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(senderProps());
+//        AdminClient adminClient = KafkaAdminClient.create(senderProps());
+//
+//        CreateTopicsResult res = adminClient.createTopics(
+//                Stream.of(KafkaTopic.values()).map(
+//                        name -> new NewTopic(name.getName(), 1, (short) 1)
+//                ).collect(Collectors.toList())
+//        );
+//
+//        return new KafkaTemplate<>(pf);
+//    }
+//
+//    private Map<String, Object> senderProps() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+//        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+//        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+//        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        return props;
+//    }
 }
